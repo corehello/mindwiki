8080ab8841ece2602da9c62470ba8ffe529efa45
weiweijiang <corehello@gmail.com>
2014-06-09 11:21:29 +0800

    Update mindwiki.js

--- mindwiki.js
+++ mindwiki.js
@@ -1,3 +1,25 @@
 var mindwiki={
   
+  base_node:Null,
+  
+  init: function(div_element){
+    base_node=div_element;
+  }
+  ,
+  insert_node: function() {
+    
+  }
+  ,
+  delete_node: function() {
+    
+  }
+  ,
+  find_node: function() {
+    
+  }
+  ,
+  update_node: function() {
+    
+  }
+    
 }
